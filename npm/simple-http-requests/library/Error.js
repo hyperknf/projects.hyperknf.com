00d9ad06a68b9617138dd2b151e99e26901be26a
HyperKNF <115628312+hyperknf@users.noreply.github.com>
2023-05-09 21:51:26 +0800

    Delete npm/simple-http-requests directory

--- npm/simple-http-requests/library/Error.js
+++
@@ -1,8 +0,0 @@
-module.exports = function (message, expected, got) {
-    function format(m, e, g) {
-        let text = `${m}`
-        if (e) text += `\n\n== Expected:\n${e}`
-        if (g) text += `\n\n== Got:\n${g}`
-    }
-    throw new Error(format(message, expected, got))
-}
--- npm/simple-http-requests/test/data.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    content: "NPM package test (by HyperKNF)"
-}
