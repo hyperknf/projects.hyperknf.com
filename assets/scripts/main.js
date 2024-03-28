f84bd91b83c511efc2c8325421cc6f7906b67451
HyperKNF <115628312+hyperknf@users.noreply.github.com>
2024-03-28 14:29:36 +0800

    Update main.js

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -3,3 +3,6 @@ console.log("Script successfully loaded")
 function SimpleHttpRequests() {
     window.location.href = "https://projects.hyperknf.com/npm/simple-http-requests"
 }
+function HydroScript() {
+    window.location.href = "https://hydroscript.hyperknf.com/"
+}
